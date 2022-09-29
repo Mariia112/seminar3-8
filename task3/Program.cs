2a887f86735d08c06852b182786aa05d6686271c
Mariia <martush1992@gmail.com>
2022-09-29 23:11:30 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,116 @@
+﻿/* Задача 58: Задайте две матрицы.
+Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+ 2 4 | 3 4
+ 3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+Console.WriteLine("Введите количество строк массива1");
+int rows1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите количество столбцов массива1");
+int columns1 = int.Parse(Console.ReadLine()!);
+
+int[,] arrayA = new int[rows1, columns1];
+
+Console.WriteLine("Введите количество строк массива2");
+int rows2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите количество столбцов массива2");
+int columns2 = int.Parse(Console.ReadLine()!);
+
+int[,] arrayB = new int[rows2, columns2];
+int[,] arrayC = new int[rows1, columns2];
+
+FillArrayA(arrayA);
+PrintArrayA(arrayA);
+Console.WriteLine();
+
+FillArrayB(arrayB);
+PrintArrayB(arrayB);
+Console.WriteLine();
+
+ProductMatrix(arrayC);
+PrintArrayC(arrayC);
+
+void FillArrayA(int[,] arrayA)
+{
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < columns1; j++)
+        {
+            arrayA[i, j] = new Random().Next(1, 5);
+
+        }
+    }
+
+}
+
+void PrintArrayA(int[,] arrayA)
+{
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < columns1; j++)
+        {
+            Console.Write($"{arrayA[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrayB(int[,] arrayB)
+{
+    for (int i = 0; i < rows2; i++)
+    {
+        for (int j = 0; j < columns2; j++)
+        {
+            arrayB[i, j] = new Random().Next(1, 5);
+
+        }
+    }
+
+}
+void PrintArrayB(int[,] arrayB)
+{
+    for (int i = 0; i < rows2; i++)
+    {
+        for (int j = 0; j < columns2; j++)
+        {
+            Console.Write($"{arrayB[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ProductMatrix(int[,] arrayC)
+{
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < columns2; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < columns1; k++)
+            {
+                sum = arrayA[i, k] * arrayB[k, j] + sum;
+            }
+            arrayC[i, j] = sum;
+        }
+    }
+}
+
+
+
+void PrintArrayC(int[,] arrayC)
+{
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < columns2; j++)
+        {
+            Console.Write($"{arrayC[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
